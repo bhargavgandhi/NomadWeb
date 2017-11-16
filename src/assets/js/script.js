350b03513f6e28bb52fcad1478fd9e138b624bf9
Bhargav Gandhi <bhargavgandhi@REA-DT-102-iMac.local>
2017-11-16 18:24:40 -0500

    Update Mobile layout pages and Add Availabilities & Details Pages

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -111,14 +111,14 @@ $(function() {
   // }
 
   if(windowWidth > 999){
-    $('.map-lists-row .map-lists').css({'opacity': '0.5', 'filter': 'grayscale(100%)', '-webkit-filter': 'grayscale(100%)'})
+    $('.destination-list .map-lists-row .map-lists').css({'opacity': '0.5', 'filter': 'grayscale(100%)', '-webkit-filter': 'grayscale(100%)'})
     $('.map-all-lists').css({'opacity': '1', 'filter': 'grayscale(0%)', '-webkit-filter': 'grayscale(0%)'})
 
     $('.map-lists .map-btns').click(function() {
       var thisTarget = $(this).data('target-image'),
         thisTargetImg = "." + thisTarget + "-image";
 
-      $('.map-lists').css({'opacity': '0.5', 'filter': 'grayscale(100%)', '-webkit-filter': 'grayscale(100%)'})
+      $('.destination-list .map-lists').css({'opacity': '0.5', 'filter': 'grayscale(100%)', '-webkit-filter': 'grayscale(100%)'})
       $(this).parent().css({'opacity': '1', 'filter': 'grayscale(0%)', '-webkit-filter': 'grayscale(0%)'})
 
       $('.map-images').css({'opacity': 0});
